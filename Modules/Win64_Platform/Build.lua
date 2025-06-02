4b74ee222cc9f89c587790cc45891b55aba88b92
jamme05 <thenewspacerabbit@gmail.com>
2025-06-02 19:11:28 +0200

    Changed naming convention

--- Modules/Win64_Platform/Build.lua
+++ Modules/Win64_Platform/Build.lua
@@ -5,7 +5,7 @@ module = {
         filter{ "platforms:Win64" }
             toolset( "clang" )
             cppdialect( "c++20" ) -- Add something like Maximum Cpp version/dialect?
-            defines( { "QW_TARGET_WIN64", "QW_CPP20" } )
+            defines( { "QW_TARGET_WIN64", "QW_CPP20", "QW_GAME_DIR=./" } )
             buildoptions { "/Zc:preprocessor" }
 
         -- May remove QW_CPP20 later.
