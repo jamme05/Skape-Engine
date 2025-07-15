2ca720c2130cf24844ffe05ad00bc4cee7edbca5
jamme05 <thenewspacerabbit@gmail.com>
2025-07-15 23:10:23 +0200

    Replaced lodepng with stb_image

---
+++ src/engine/Helpers/stb_image_wrapper.h
@@ -0,0 +1,14 @@
+﻿/*
+ *
+ * COPYRIGHT William Ask S. Ness 2025
+ *
+ */
+
+#pragma once
+
+#define STBI_MALLOC( S )     SK_ALLOC( S )
+#define STBI_REALLOC( B, S ) SK_REALLOC( B, S )
+#define STBI_FREE( B )       SK_FREE( B )
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
