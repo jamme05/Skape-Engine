6121232ea71a525c245ca254a680e7a635f3f3bc
jamme05 <thenewspacerabbit@gmail.com>
2026-01-11 01:23:00 +0100

    Updated Typed Graphics Buffer functionality

--- src/engine/Graphics/Buffer/Buffer.h
+++ src/engine/Graphics/Buffer/Buffer.h
@@ -6,7 +6,9 @@
 
 #pragma once
 
+
 #include <Graphics/Buffer/Unsafe_Buffer.h>
+#include <Math/Math.h>
 #include <Memory/Memory.h>
 
 // TYPE SAFE BUFFER
@@ -31,44 +33,60 @@ namespace sk::Graphics
         cBuffer( const cBuffer& _other );
         cBuffer( cBuffer&& _other ) noexcept;
         ~cBuffer();
+        
+        static constexpr auto getByteSize( const size_t _count ){ return Memory::get_aligned( _count * kTypeSize, Memory::kShaderAlign ); }
 
-        // Skip align or use it?
-        static constexpr auto getSize( const size_t _count ){ return _count * kTypeSize; }
-
-        auto data() -> Ty*;
-        auto data() const -> const Ty*;
-
+        auto Data() -> Ty*;
+        auto Data() const -> const Ty*;
+        
+        // TODO: Add capacity control once it's been added to the unsafe buffer
+        auto Size    () const -> size_t;
+        auto Capacity() const -> size_t;
+        
+        void Resize( size_t _new_size );
+        void Reserve( size_t _new_capacity );
+        
+        // Returns the index of the inserted value
+        template< class... Args >
+        requires std::constructible_from< Ty, Args... >
+        size_t EmplaceBack( Args&&... _value );
+        void   PopBack( Ty* _removed_value );
+        
+        auto Front() -> Ty&;
+        auto Front() const -> const Ty&;
+        
+        auto Back() -> Ty&;
+        auto Back() const -> const Ty&;
+        
         auto operator=( const cBuffer& _other ) -> cBuffer&;
         cBuffer& operator=( cBuffer&& _other ) noexcept;
 
         auto operator[]( size_t _index ) -> Ty&;
         auto operator[]( size_t _index ) const -> const Ty&;
     private:
-        struct sData
-        {
-            size_t size;
-            size_t capacity;
-        } m_data_;
+        size_t         m_size_;
         cUnsafe_Buffer m_buffer_;
     };
     
     // TODO: Is normalized support.
     template< class Ty, Buffer::eType Type >
     cBuffer< Ty, Type >::cBuffer( const std::string& _name, const bool _is_static )
-    : m_buffer_( _name, 0, sizeof( Ty ), Type, false, _is_static )
+    : m_size_( 0 )
+    , m_buffer_( _name, 0, kTypeSize, Type, false, _is_static )
     {} // cBuffer
 
     template< class Ty, Buffer::eType Type >
     cBuffer< Ty, Type >::cBuffer( const std::string& _name, const size_t _size, const bool _is_static )
-    : m_buffer_( _name, _size * sizeof( Ty ), sizeof( Ty ), Type, false, _is_static )
+    : m_size_( _size )
+    , m_buffer_( _name, getByteSize( _size ), kTypeSize, Type, false, _is_static )
     {} // cBuffer
 
     template< class Ty, Buffer::eType Type >
     cBuffer< Ty, Type >::cBuffer( const std::string& _name, std::initializer_list< Ty > _values, const bool _is_static )
-    : m_buffer_( _name, getSize( _values.size() ), Type, _is_static )
+    : m_size_( _values.size() )
+    , m_buffer_( _name, getByteSize( _values.size() ), Type, _is_static )
     {
-        auto size = getSize( _values.size() );
-        m_buffer_.Update( _values.begin(), size );
+        m_buffer_.Update( _values.begin(), _values.size() );
     } // cBuffer
 
     template< class Ty, Buffer::eType Type >
@@ -78,6 +96,7 @@ namespace sk::Graphics
     {
         const auto dist = std::distance( _begin, _end );
         m_buffer_ = { _name, dist, Type, _is_static };
+        m_size_ = dist;
 
         std::vector< Ty > copy( _begin, _end );
         m_buffer_.Update( copy.data(), dist * kTypeSize );
@@ -85,12 +104,14 @@ namespace sk::Graphics
 
     template< class Ty, Buffer::eType Type >
     cBuffer< Ty, Type >::cBuffer( const cBuffer& _other )
-    : m_buffer_( _other.m_buffer_ )
+    : m_size_( _other.m_size_ )
+    , m_buffer_( _other.m_buffer_ )
     {} // cBuffer ( Copy )
 
     template< class Ty, Buffer::eType Type >
     cBuffer< Ty, Type >::cBuffer( cBuffer&& _other ) noexcept
-    : m_buffer_( std::move( _other.m_buffer_ ) )
+    : m_size_( _other.m_size_ )
+    , m_buffer_( std::move( _other.m_buffer_ ) )
     {} // cBuffer ( Move )
 
     template< class Ty, Buffer::eType Type >
@@ -100,16 +121,91 @@ namespace sk::Graphics
     } // ~cBuffer
 
     template< class Ty, Buffer::eType Type >
-    auto cBuffer< Ty, Type >::data() -> Ty*
+    auto cBuffer< Ty, Type >::Data() -> Ty*
     {
         return static_cast< Ty* >( m_buffer_.Data() );
     } // data
 
     template< class Ty, Buffer::eType Type >
-    auto cBuffer< Ty, Type >::data() const -> const Ty*
+    auto cBuffer< Ty, Type >::Data() const -> const Ty*
     {
         return static_cast< const Ty* >( m_buffer_.Data() );
     } // data
+    
+    template< class Ty, Buffer::eType Type >
+    auto cBuffer< Ty, Type >::Size() const -> size_t
+    {
+        return m_size_;
+    }
+
+    template< class Ty, Buffer::eType Type >
+    auto cBuffer< Ty, Type >::Capacity() const -> size_t
+    {
+        return 0;
+    }
+
+    template< class Ty, Buffer::eType Type >
+    void cBuffer< Ty, Type >::Resize( const size_t _new_size )
+    {
+        if( _new_size < m_size_ )
+            return;
+        
+        m_buffer_.Resize( getByteSize( _new_size ) );
+    }
+
+    template< class Ty, Buffer::eType Type >
+    void cBuffer< Ty, Type >::Reserve( const size_t _new_capacity )
+    {
+        // TODO: Add the reserve function
+    }
+
+    template< class Ty, Buffer::eType Type >
+    template< class... Args >
+    requires std::constructible_from< Ty, Args... >
+    size_t cBuffer< Ty, Type >::EmplaceBack( Args&&... _value )
+    {
+        auto index = Size();
+        Resize( index + 1 );
+        
+        std::construct_at( Data + index, std::forward< Args >( _value )... );
+        ::new( Data() + index ) Ty{ std::forward< Args >( _value )... };
+        return index;
+    }
+
+    template< class Ty, Buffer::eType Type >
+    void cBuffer< Ty, Type >::PopBack( Ty* _removed_value )
+    {
+        auto index = Size() - 1;
+        
+        if( _removed_value != nullptr )
+            _removed_value = Data()[ index ];
+        
+        Resize( index );
+    }
+
+    template< class Ty, Buffer::eType Type >
+    auto cBuffer< Ty, Type >::Front() -> Ty&
+    {
+        return *Data();
+    }
+
+    template< class Ty, Buffer::eType Type >
+    auto cBuffer< Ty, Type >::Front() const -> const Ty&
+    {
+        return *Data();
+    }
+
+    template< class Ty, Buffer::eType Type >
+    auto cBuffer< Ty, Type >::Back() -> Ty&
+    {
+        return Data()[ Size() - 1 ];
+    }
+
+    template< class Ty, Buffer::eType Type >
+    auto cBuffer< Ty, Type >::Back() const -> const Ty&
+    {
+        return Data()[ Size() - 1 ];
+    }
 
     template< class Ty, Buffer::eType Type >
     auto cBuffer< Ty, Type >::operator=( const cBuffer& _other ) -> cBuffer&
@@ -132,13 +228,13 @@ namespace sk::Graphics
     template< class Ty, Buffer::eType Type >
     auto cBuffer< Ty, Type >::operator[]( size_t _index ) -> Ty&
     {
-        return data()[ _index ];
+        return Data()[ _index ];
     } // operator[]
 
     template< class Ty, Buffer::eType Type >
     auto cBuffer< Ty, Type >::operator[]( size_t _index ) const -> const Ty&
     {
-        return data()[ _index ];
+        return Data()[ _index ];
     } // operator[] const
     
     template< class Ty >
