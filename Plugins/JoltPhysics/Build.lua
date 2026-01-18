295eb5ddb04c5f3eaf1007e2977322aec87844a7
jamme05 <thenewspacerabbit@gmail.com>
2026-01-18 23:43:54 +0100

    Added initial files for Jolt Physics plugin

--- Plugins/JoltPhysics/Build.lua
+++ Plugins/JoltPhysics/Build.lua
@@ -1,11 +1,33 @@
+local jolt_dir = "JoltPhysics"
+local jolt_include_dir = path.join( jolt_dir, "Jolt" )
+local jolt_asset_dir = path.join( jolt_dir, "Assets" )
+local jolt_build_dir = "Build/Plugins/JoltPhysics/JoltPhysics/cmake/Release/"
 
-module = {
-    Name = "Jolt",
-    Setup_Workspace = function()
+plugin = {
+    Name = "Jolt Physics",
+    Setup_Workspace = function( plugin )
+        filter( plugin.Filter )
         defines( { "QW_PHYSICS_JOLT" } )
     end,
 
-    Module_Project = Default_Module_Setup( "Jolt" )
-}
+    Init = function( plugin_dir )
+        CMakeBuilder( path.join( plugin_dir, jolt_dir ), "Build", "Release" )
 
--- Probably not needed?
+        CopyFolder( path.join( plugin_dir, jolt_asset_dir ), path.join( jolt_build_dir, "Assets" ) )
+    end,
+
+    IncludeLibs = function( plugin_dir )
+        return {
+            path.join( jolt_build_dir, "Jolt.lib" ),
+        }
+    end,
+
+    -- TODO: Add cache.
+    IncludeDirs = function( plugin_dir )
+        return { path.join( "Plugins", plugin_dir, jolt_include_dir ) }
+    end,
+}
+plugin.Plugin_Project = Plugin_Setup( "Jolt Physics",
+    plugin.IncludeLibs,
+    plugin.IncludeDirs
+)
