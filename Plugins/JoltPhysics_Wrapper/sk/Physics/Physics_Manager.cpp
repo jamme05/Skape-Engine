011c9751903245b0701a16e3912d5c89340da03d
William Ask Ness <willness@playgroundsquad.com>
2026-01-26 21:40:23 +0100

    Jolt CMake setup should be done

--- Plugins/JoltPhysics_Wrapper/sk/Physics/Physics_Manager.cpp
+++ Plugins/JoltPhysics_Wrapper/sk/Physics/Physics_Manager.cpp
@@ -112,12 +112,12 @@ public:
 	}
 
 #if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
-	virtual const char *			GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const override
+	virtual const char *			GetBroadPhaseLayerName(JPH::BroadPhaseLayer inLayer) const override
 	{
-		switch ((BroadPhaseLayer::Type)inLayer)
+		switch ((JPH::BroadPhaseLayer::Type)inLayer)
 		{
-		case (BroadPhaseLayer::Type)BroadPhaseLayers::NON_MOVING:	return "NON_MOVING";
-		case (BroadPhaseLayer::Type)BroadPhaseLayers::MOVING:		return "MOVING";
+		case (JPH::BroadPhaseLayer::Type)BroadPhaseLayers::NON_MOVING:	return "NON_MOVING";
+		case (JPH::BroadPhaseLayer::Type)BroadPhaseLayers::MOVING:		return "MOVING";
 		default:													JPH_ASSERT(false); return "INVALID";
 		}
 	}
