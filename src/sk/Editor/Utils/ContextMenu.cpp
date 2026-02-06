da0f98fdafec5e7b0e4e6e017fca6c95f1599018
William Ask Ness <willness@playgroundsquad.com>
2026-02-06 17:56:40 +0100

    Added branching and disabling/enabling to context menu

--- src/sk/Editor/Utils/ContextMenu.cpp
+++ src/sk/Editor/Utils/ContextMenu.cpp
@@ -7,6 +7,9 @@
 #include "ContextMenu.h"
 
 #include <imgui.h>
+#include <ranges>
+
+#include "sk/Debugging/Macros/Assert.h"
 
 using namespace sk::Editor::Utils;
 
@@ -16,6 +19,13 @@ namespace
     template< class... Ts >
     struct sVisitor : Ts... { using Ts::operator()...; };
 } // ::
+
+cContextMenu::cContextMenu()
+{
+    m_active_builder_stack_.emplace( eActiveBuilder::kSelf );
+}
+
+cContextMenu::~cContextMenu() = default;
 
 void cContextMenu::Draw( const std::string_view& _name )
 {
@@ -38,71 +48,313 @@ void cContextMenu::Draw( const std::string_view& _name )
 
 auto cContextMenu::Add( const std::string& _text ) -> cContextMenu&
 {
-    m_items_.emplace_back( _text, std::monostate{} );
+    _add( sItem{ _text, std::monostate{} } );
 
     return *this;
 }
 
 auto cContextMenu::Add( const std::string& _name, bool& _bool_value ) -> cContextMenu&
 {
-    m_items_.emplace_back( _name, &_bool_value );
+    _add( sItem{ _name, &_bool_value } );
 
     return *this;
 }
 
 auto cContextMenu::Add( const std::string& _name, const callback_t& _callback ) -> cContextMenu&
 {
-    m_items_.emplace_back( _name, _callback );
+    _add( sItem{ _name, _callback } );
 
     return *this;
 }
 
-auto cContextMenu::AddMenu( const std::string& _name ) -> cContextMenu
+auto cContextMenu::If( const predicate_t& _predicate ) -> cContextMenu&
 {
-    m_items_.emplace_back( _name, std::monostate{} );
+    auto builder = new sIfBuilder{};
+    m_if_builders_.emplace( builder );
+    builder->statements.emplace_back( _predicate );
 
-    cContextMenu temp_menu{};
-    temp_menu.m_parent_menu_ = this;
+    m_active_builder_stack_.emplace( eActiveBuilder::kIf );
 
-    return temp_menu;
+    return *this;
 }
 
-auto cContextMenu::Submit() -> cContextMenu&
+auto cContextMenu::ElseIf( const predicate_t& _predicate ) -> cContextMenu&
 {
-    m_parent_menu_->m_items_.back().second = *this;
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() == eActiveBuilder::kElse,
+        "Error: You're not allowed to add more branches after else.", *this )
+
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() != eActiveBuilder::kIf,
+        "Error: Context menu isn't currently building an if statement.", *this )
+
+    auto& builder = *m_if_builders_.top();
+    builder.active_if_index++;
+    builder.statements.emplace_back( _predicate );
+
+    return *this;
+}
+
+auto cContextMenu::Else() -> cContextMenu&
+{
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() == eActiveBuilder::kElse,
+        "Error: You're not allowed to add more branches after else.", *this )
+
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() != eActiveBuilder::kIf,
+        "Error: Context menu isn't currently building an if statement.", *this )
+
+    m_if_builders_.top()->active_if_index = -1;
+
+    return *this;
+}
+
+auto cContextMenu::EndIf() -> cContextMenu&
+{
+    SK_BREAK_RET_IFN( sk::Severity::kEditor, _getActiveBuilder() == eActiveBuilder::kIf || _getActiveBuilder() == eActiveBuilder::kElse,
+        "Error: Context menu isn't currently building an if statement.", *this )
+
+    auto& builder = *m_if_builders_.top();
+
+    auto  branch_ptr = std::make_unique< sBranch >();
+    auto& branch  = *branch_ptr;
+    branch.selection = [ statements = builder.statements ]( void* _user_data )
+    {
+        for( size_t i = 0; i < statements.size(); i++ )
+        {
+            if( statements[ i ]( _user_data ) )
+                return static_cast< int_fast32_t >( i );
+        }
+        return -1;
+    };
+    branch.default_selection = -1;
+    branch.value_map.swap( builder.values_map );
+
+    delete &builder;
+    m_active_builder_stack_.pop();
+    m_if_builders_.pop();
+
+    _add( { {}, std::move( branch_ptr ) } );
+
+    return *this;
+}
+
+auto cContextMenu::Switch( const selection_t& _selection ) -> cContextMenu&
+{
+    auto builder = new sSwitchBuilder{};
+    m_switch_builder_stack_.emplace( builder );
+    builder->selection = _selection;
+
+    m_active_builder_stack_.emplace( eActiveBuilder::kSwitch );
+
+    return *this;
+}
+
+auto cContextMenu::Case( uint_fast32_t _value ) -> cContextMenu&
+{
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() != eActiveBuilder::kSwitch,
+        "Error: Context menu isn't currently building a switch statement.", *this )
+
+    auto& builder = *m_switch_builder_stack_.top();
+
+    builder.active_switch_case = _value;
+    builder.using_default_case = false;
+
+    return *this;
+}
+
+auto cContextMenu::Default() -> cContextMenu&
+{
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() != eActiveBuilder::kSwitch,
+        "Error: Context menu isn't currently building a switch statement.", *this )
+
+    auto& builder = *m_switch_builder_stack_.top();
+    builder.using_default_case = true;
+
+    return *this;
+}
+
+auto cContextMenu::EndSwitch() -> cContextMenu&
+{
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() != eActiveBuilder::kSwitch,
+        "Error: Context menu isn't currently building a switch statement.", *this )
+
+    auto& builder = *m_switch_builder_stack_.top();
+    auto  branch_ptr = std::make_unique< sBranch >();
+    auto& branch  = *branch_ptr;
+
+    auto default_index = ( --builder.value_map.end() )->first + 1;
+    while( builder.value_map.contains( default_index ) )
+        default_index++;
+
+    branch.selection = builder.selection;
+    branch.default_selection = default_index;
+    branch.value_map.swap( builder.value_map );
+    for( auto& value : builder.default_values )
+        branch.value_map.emplace( default_index, std::move( value ) );
+
+    _add( { {}, std::move( branch_ptr ) } );
+
+    delete &builder;
+    m_switch_builder_stack_.pop();
+    m_active_builder_stack_.pop();
+
+    return *this;
+}
+
+auto cContextMenu::AddSubMenu( const std::string& _name ) -> cContextMenu&
+{
+    auto temp_menu = std::make_unique< cContextMenu >();
+    temp_menu->m_parent_menu_ = this;
+
+    auto ptr = temp_menu.get();
+
+    _add( { _name, std::move( temp_menu ) } );
+
+    m_active_builder_stack_.emplace( eActiveBuilder::kChild );
+
+    return *ptr;
+}
+
+auto cContextMenu::EndSubMenu() -> cContextMenu&
+{
+    SK_BREAK_RET_IF( sk::Severity::kEditor, _getActiveBuilder() != eActiveBuilder::kSelf && m_parent_menu_ != nullptr,
+        "Error: This context menu isn't valid for this call.", *this )
+
+    Complete();
+
+    m_parent_menu_->m_active_builder_stack_.pop();
 
     return *m_parent_menu_;
 }
 
+auto cContextMenu::SetDisabled( bool _is_disabled ) -> cContextMenu&
+{
+    _add( { {}, _is_disabled ? eSetting::kSetDisabled : eSetting::kSetEnabled } );
+
+    return *this;
+}
+
+void cContextMenu::Complete()
+{
+    SK_BREAK_IF( sk::Severity::kEngine, m_active_builder_stack_.top() != eActiveBuilder::kSelf,
+        "Error: " )
+
+    m_active_builder_stack_.pop();
+}
+
 void cContextMenu::SetUserData( void* _data )
 {
+    SK_BREAK_RET_IF( sk::Severity::kEditor, !IsValid(), "Error: Context menu isn't properly initialized." )
+
     m_user_data_ = _data;
+}
+
+bool cContextMenu::IsValid() const
+{
+    return m_active_builder_stack_.empty();
+}
+
+auto cContextMenu::_getActiveBuilder() -> eActiveBuilder
+{
+    return m_active_builder_stack_.empty() ? eActiveBuilder::kNone : m_active_builder_stack_.top();
 }
 
 void cContextMenu::_drawItems()
 {
-    for( auto& [ name , value ] : m_items_ )
-    {
-        sVisitor visitor{
-            [&]( std::monostate& )
-            {
-                ImGui::Text( "%s", name.c_str() );
-            },
-            [&]( bool*& _bool_value )
-            {
-                ImGui::Checkbox( name.c_str(), _bool_value );
-            },
-            [&]( const callback_t& _callback )
-            {
-                if( ImGui::MenuItem( name.c_str() ) )
-                    _callback( m_user_data_ );
-            },
-            [&]( cContextMenu& _submenu )
-            {
-                _submenu.Draw( name );
-            }
-        };
+    SK_BREAK_IF( sk::Severity::kEditor,
+        _getActiveBuilder() == eActiveBuilder::kSelf,
+        "Error: You forgot to call Complete on ContextMenu." )
 
-        std::visit( visitor, value );
+    SK_BREAK_RET_IF( sk::Severity::kEditor, !IsValid(), "Error: Context menu isn't properly initialized." )
+
+    m_currently_disabled_ = false;
+
+    for( auto& item : m_items_ )
+        _drawItem( item );
+
+    if( m_currently_disabled_ )
+        ImGui::EndDisabled();
+}
+
+void cContextMenu::_drawItem( sItem& _item )
+{
+    auto& [ name, value ] = _item;
+
+    sVisitor visitor{
+        [&]( std::monostate& )
+        {
+            ImGui::Text( "%s", name.c_str() );
+        },
+        [&]( bool*& _bool_value )
+        {
+            ImGui::Checkbox( name.c_str(), _bool_value );
+        },
+        [&]( const callback_t& _callback )
+        {
+            if( ImGui::MenuItem( name.c_str() ) )
+                _callback( m_user_data_ );
+        },
+        [&]( const eSetting& _settings )
+        {
+            switch( _settings ) {
+            case eSetting::kSetDisabled:
+                ImGui::BeginDisabled();
+                m_currently_disabled_ = true;
+                break;
+            case eSetting::kSetEnabled:
+                ImGui::EndDisabled();
+                m_currently_disabled_ = false;
+                break;
+            }
+        },
+        [&]( const std::unique_ptr< cContextMenu >& _submenu )
+        {
+            _submenu->Draw( name );
+        },
+        [&]( const std::unique_ptr< sBranch >& _branch )
+        {
+            auto& value_map = _branch->value_map;
+            const auto result = _branch->selection( m_user_data_ );
+
+            auto [ fst, lst ] = value_map.equal_range( result );
+            if( fst == value_map.end() )
+            {
+                auto [ default_fst, default_snd] = value_map.equal_range( _branch->default_selection );
+                fst = default_fst;
+                lst = default_snd;
+            }
+
+            for( ; fst != lst; ++fst )
+                _drawItem( fst->second );
+        }
+    };
+
+    std::visit( visitor, value );
+}
+
+void cContextMenu::_add( sItem&& _value )
+{
+    switch( _getActiveBuilder() )
+    {
+    case eActiveBuilder::kChild:
+    case eActiveBuilder::kNone:
+    {
+        SK_BREAK_IF( sk::Severity::kEditor, true, "You're not supposed to add anything at this time." )
+    }
+    break;
+    case eActiveBuilder::kSelf:
+        m_items_.emplace_back( std::move( _value ) );
+    break;
+    case eActiveBuilder::kIf:
+    {
+        auto& builder = *m_if_builders_.top();
+        builder.values_map.emplace( builder.active_if_index, std::move( _value ) );
+    }
+    break;
+    case eActiveBuilder::kSwitch:
+    {
+        auto& builder = *m_switch_builder_stack_.top();
+        builder.value_map.emplace( builder.active_switch_case, std::move( _value ) );
+    }
+    break;
+    default: break;
     }
 }
--- src/sk/Editor/Utils/ContextMenu.h
+++ src/sk/Editor/Utils/ContextMenu.h
@@ -6,6 +6,8 @@
 #pragma once
 
 #include <functional>
+#include <map>
+#include <stack>
 #include <string_view>
 #include <variant>
 #include <vector>
@@ -14,10 +16,36 @@ namespace sk::Editor::Utils
 {
     class cContextMenu
     {
+        enum class eSetting
+        {
+            kSetDisabled,
+            kSetEnabled,
+        };
     public:
-        using callback_t = std::function< void( void* ) >;
-        using value_t    = std::variant< std::monostate, bool*, callback_t, cContextMenu >;
+        struct sBranch;
+        using predicate_t = std::function< bool( void* ) >;
+        using selection_t = std::function< int_fast32_t( void* ) >;
+        using callback_t  = std::function< void( void* ) >;
+        using value_t     = std::variant< std::monostate, bool*, callback_t, eSetting, std::unique_ptr< cContextMenu >, std::unique_ptr< sBranch > >;
 
+        struct sItem
+        {
+            std::string name;
+            value_t     value;
+        };
+
+        struct sBranch
+        {
+            using range_t     = std::pair< uint32_t, uint32_t >;
+            using range_vec_t = std::vector< range_t >;
+            using value_map_t = std::multimap< int_fast32_t, sItem >;
+            selection_t  selection;
+            int_fast32_t default_selection;
+            value_map_t  value_map;
+        };
+
+        cContextMenu();
+        ~cContextMenu();
 
         void Draw( const std::string_view& _name = {} );
 
@@ -29,25 +57,99 @@ namespace sk::Editor::Utils
         // Adds a button item to the menu
         auto Add( const std::string& _name, const callback_t& _callback ) -> cContextMenu&;
 
+        // Adds a button with the user data already cast. DO REMEMBER TO KEEP TRACK OF THE TYPE
+        template< class Ty >
+        auto Add( const std::string& _name, const std::function< void( Ty* ) >& _callback ) -> cContextMenu&
+        {
+            return Add( _name, [ _callback ]( void* _data ){ _callback( static_cast< Ty* >( _data ) ); } );
+        }
+
+        auto If    ( const predicate_t& _predicate ) -> cContextMenu&;
+        template< class Ty >
+        auto If( const std::function< bool( Ty* ) >& _predicate ) -> cContextMenu&
+        {
+            return If( [ _predicate ]( void* _data ){ return _predicate( static_cast< Ty* >( _data ) ); } );
+        }
+        auto ElseIf( const predicate_t& _predicate ) -> cContextMenu&;
+        template< class Ty >
+        auto ElseIf( const std::function< bool( Ty* ) >& _predicate ) -> cContextMenu&
+        {
+            return ElseIf( [ _predicate ]( void* _data ){ return _predicate( static_cast< Ty* >( _data ) ); } );
+        }
+        auto Else  () -> cContextMenu&;
+        auto EndIf () -> cContextMenu&;
+
+        auto Switch   ( const selection_t& _selection ) -> cContextMenu&;
+        template< class Ty >
+        auto Switch( const std::function< int_fast32_t( Ty* ) >& _selection ) -> cContextMenu&
+        {
+            return If( [ _selection ]( void* _data ){ return _selection( static_cast< Ty* >( _data ) ); } );
+        }
+        auto Case     ( uint_fast32_t _value ) -> cContextMenu&;
+        auto Default  () -> cContextMenu&;
+        auto EndSwitch() -> cContextMenu&;
+
         /**
-         *
+         * You are not required to call Complete on the submenu.
          * @param _name The name of the submenu
          * @return The submenu to fill with content
          */
-        auto AddMenu( const std::string& _name ) -> cContextMenu;
+        auto AddSubMenu( const std::string& _name ) -> cContextMenu&;
         /**
          *
          * @return A reference to the parent menu
          */
-        auto Submit() -> cContextMenu&;
+        auto EndSubMenu() -> cContextMenu&;
+
+        auto SetDisabled( bool _is_disabled ) -> cContextMenu&;
+
+        void Complete();
 
         void SetUserData( void* _data );
 
+        bool IsValid() const;
+
     private:
+        enum class eActiveBuilder
+        {
+            kNone,
+            kSelf,
+            kIf,
+            kElse,
+            kSwitch,
+            kChild
+        };
+        struct sIfBuilder
+        {
+            using branches_t = std::vector< predicate_t >;
+            using value_map_t = std::multimap< int_fast32_t, sItem >;
+            size_t      active_if_index;
+            branches_t  statements;
+            value_map_t values_map;
+        };
+        struct sSwitchBuilder
+        {
+            using value_map_t = std::multimap< int_fast32_t, sItem >;
+            using value_vec_t = std::vector< sItem >;
+            int_fast32_t active_switch_case;
+            bool         using_default_case;
+            selection_t  selection;
+            value_map_t  value_map;
+            value_vec_t  default_values;
+        };
+        auto _getActiveBuilder() -> eActiveBuilder;
         void _drawItems();
+        void _drawItem( sItem& _item );
+        void _add( sItem&& _value );
+
+        std::stack< eActiveBuilder >  m_active_builder_stack_;
+        std::stack< sSwitchBuilder* > m_switch_builder_stack_;
+        std::stack< sIfBuilder* >     m_if_builders_;
 
         cContextMenu* m_parent_menu_ = nullptr;
         void*         m_user_data_   = nullptr;
-        std::vector< std::pair< std::string, value_t > > m_items_;
+        std::vector< sItem > m_items_;
+
+        bool m_currently_disabled_ = false;
     };
 } // sk::Editor::Utils::
